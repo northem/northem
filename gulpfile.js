0dd3834c8017f783c44c3d439a93ff8f68344ce6
Arctic Ice Studio <development@arcticicestudio.com>
2016-10-28 22:33:29 +0200

    GHI-#9 Implement the gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,142 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+title      Gulp Project File                          +
+project    northem                                    +
+version                                               +
+repository https://github.com/arcticicestudio/northem +
+author     Arctic Ice Studio                          +
+email      development@arcticicestudio.com            +
+copyright  Copyright (C) 2016                         +
++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+[Index]
+  > Configuration
+  > Imports
+  > Tasks
+      clean
+      clean-css
+      clean-documentation
+      compile-css-template
+      default
+      sassdoc
+
+[References]
+Gulp
+  (http://gulpjs.com)
+npmjs
+  (https://www.npmjs.com)
+*/
+"use strict";
+/*+---------------+
+  + Configuration +
+  +---------------+*/
+var config = {
+  project: {
+    id: "northem",
+    name: "Northem",
+    version: "",
+    repository: "https://github.com/arcticicestudio/northem",
+    author: "Arctic Ice Studio",
+    email: "development@arcticicestudio.com"
+  },
+  build: {
+    base: "./build",
+    css: "./build/css",
+    sassdoc: "./build/documentation/sassdoc"
+  },
+  src: {
+    sass: "./src/sass"
+  },
+  tasks: {
+    compilation: {
+      sass: {
+        extensions: {
+          input: ["scss"],
+          output: "css"
+        },
+        options: {
+          indentedSyntax: false,
+          indentType: "space",
+          indentWidth: 2,
+          linefeed: "lf",
+          outputStyle: "expanded",
+          precision: 5,
+          sourceComments: false
+        }
+      }
+    }
+  }
+}
+
+/*+---------+
+  + Imports +
+  +---------+*/
+var del      = require("del");
+var fs       = require("fs");
+var gulp     = require("gulp-help")(require("gulp"));
+var gulputil = require("gulp-util");
+var path     = require("path");
+var plumber  = require("gulp-plumber");
+var rename   = require("gulp-rename");
+var sass     = require("gulp-sass");
+var sassdoc  = require("sassdoc");
+
+/*+-------+
+  + Tasks +
+  +-------+*/
+/**
+ * Cleans the whole build folder.
+ *
+ * @since 2.0.0
+ */
+gulp.task("clean", function() {
+  del(config.build.base);
+});
+
+/**
+ * Cleans the CSS build folder.
+ *
+ * @since 2.0.0
+ */
+gulp.task("clean-css", function(){
+  del(config.build.css);
+});
+
+/**
+ * Cleans the documentation build folder.
+ *
+ * @since 2.0.0
+ */
+gulp.task("clean-documentation", function(){
+  del(config.build.sassdoc);
+});
+
+/**
+ * Compiles the Sass CSS template.
+ *
+ * @since 2.0.0
+ */
+gulp.task("compile-css-template", function() {
+  return gulp.src(path.join(config.src.sass, "/template-css." + config.tasks.compilation.sass.extensions.input))
+    .pipe(plumber())
+    .pipe(sass(config.tasks.compilation.sass.options).on("error", sass.logError))
+    .pipe(rename(config.project.id + config.tasks.compilation.sass.extensions.output))
+    .pipe(gulp.dest(config.build.css))
+});
+
+/**
+ * Shows the help.
+ *
+ * @since 2.0.0
+ */
+gulp.task("default", ["help"]);
+
+/**
+ * Creates the Sassdoc documentation.
+ *
+ * @since 2.0.0
+ */
+gulp.task("sassdoc", function () {
+  return gulp.src(path.join(config.src.sass, "/**/*." + config.tasks.compilation.sass.extensions.input))
+    .pipe(sassdoc())
+});
